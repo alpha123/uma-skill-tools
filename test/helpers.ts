6a40185bebfa59c69e60e61766c38e10af52322d
pecan <turkchess123@gmail.com>
2022-07-20 02:01:57 -0600

    javascript sort is lexographic for numbers by default (???)
    what in the world

--- test/helpers.ts
+++ test/helpers.ts
@@ -3,21 +3,14 @@ import { prop, forAll } from './TestHelpers';
 
 import { CourseHelpers } from '../CourseData';
 
-prop('array with strictly increasing elements is sorted', forAll(fc.array(fc.nat(9999)), xs => {
+prop('array with strictly increasing elements is sorted', forAll(fc.array(fc.nat()), xs => {
 	let acc = -1;
 	const a = xs.map(x => ({start: acc += x + 1}));
 	return CourseHelpers.isSortedByStart(a);
 }));
 
-// limit to 9999 because
-//     >> [ 164202479, 999999999  ].sort()
-//        Array [ 164202479, 999999999 ]
-//     >> [ 164202479, 1000000000 ].sort()
-//        Array [ 1000000000, 164202479 ]
-// idk
-
-prop('array with not strictly increasing elements is not sorted', forAll(fc.array(fc.nat(9999), {minLength: 2}), xs => {
-	xs.sort();
+prop('array with not strictly increasing elements is not sorted', forAll(fc.array(fc.nat(), {minLength: 2}), xs => {
+	xs.sort((a,b) => +(a>b) - +(b>a));
 	const sorted = xs.slice();
 	while (xs.every((x,i) => x === sorted[i])) {
 		for (let i = xs.length; --i >= 0;) {
