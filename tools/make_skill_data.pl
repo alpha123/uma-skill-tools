alpha123/uma-skill-tools

commit 9c4f1f631553e901b851e2a4e7971512b30d1f93

track skills with multiple effects as one skill in the race solver. also add skill rarity.
this is a fairly invasive change since formerly we simply decomposed composite skills into several skills with a single effect and also didn't track any metadata such as rarity. as preparation for implementing summer goldship unique we can't do this anymore (since that should select a composite skill as one skill) and also have to track skill rarity.

tools/make_skill_data.pl
@@ -18,7 +18,7 @@ my $db = DBI->connect("dbi:SQLite:$mastermdb", undef, undef, {
 $db->{RaiseError} = 1;
 
 my $select = $db->prepare(<<SQL
-SELECT id,
+SELECT id, rarity,
        precondition_1, condition_1,
        float_ability_time_1,
        ability_type_1_1, float_ability_value_1_1,
@@ -30,14 +30,15 @@ SELECT id,
        ability_type_2_1, float_ability_value_2_1,
        ability_type_2_2, float_ability_value_2_2,
        ability_type_2_3, float_ability_value_2_3
-FROM skill_data;
+  FROM skill_data
+ WHERE is_general_skill = 1;
 SQL
 );
 
 $select->execute;
 
 my (
-	$id,
+	$id, $rarity,
 	$precondition_1, $condition_1,
 	$float_ability_time_1,
 	$ability_type_1_1, $float_ability_value_1_1,
@@ -52,7 +53,7 @@ my (
 );
 
 $select->bind_columns(\(
-	$id,
+	$id, $rarity,
 	$precondition_1, $condition_1,
 	$float_ability_time_1,
 	$ability_type_1_1, $float_ability_value_1_1,
@@ -96,7 +97,7 @@ while ($select->fetch) {
 			effects => \@effects_2
 		};
 	}
-	$skills->{$id} = \@triggers;
+	$skills->{$id} = {rarity => $rarity, alternatives => \@triggers};
 }
 
 my $json = JSON::PP->new;

RaceSolver.ts
@@ -70,17 +70,26 @@ export type DynamicCondition = (state: RaceState) => boolean;
 
 export const enum SkillType { TargetSpeed, Accel, CurrentSpeed }
 
+export const enum SkillRarity { White = 1, Gold, Unique }
+
 export interface SkillEffect {
-	skillId: string
 	type: SkillType
 	baseDuration: number
 	modifier: number
 }
 
 export interface PendingSkill {
+	skillId: string
+	rarity: SkillRarity
 	trigger: Region
 	extraCondition: DynamicCondition
-	effect: SkillEffect
+	effects: SkillEffect[]
+}
+
+interface ActiveSkill {
+	skillId: string
+	remainingDuration: number
+	modifier: number
 }
 
 export class RaceSolver {
@@ -95,16 +104,16 @@ export class RaceSolver {
 	startDash: boolean
 	phase: Phase
 	nextPhaseTransition: number
-	activeSpeedSkills: {remainingDuration: number, effect: SkillEffect}[]
-	activeAccelSkills: {remainingDuration: number, effect: SkillEffect}[]
+	activeSpeedSkills: ActiveSkill[]
+	activeAccelSkills: ActiveSkill[]
 	pendingSkills: PendingSkill[]
 	currentSpeedModifier: number
 	nHills: number
 	hillIdx: number
 	hillStart: number[]
 	hillEnd: number[]
-	onSkillActivate: (s: SkillEffect) => void
-	onSkillDeactivate: (s: SkillEffect) => void
+	onSkillActivate: (s: string) => void
+	onSkillDeactivate: (s: string) => void
 
 	constructor(horse: HorseParameters, course: CourseData) {
 		this.horse = horse;
@@ -189,7 +198,7 @@ export class RaceSolver {
 			this.targetSpeed = baseTargetSpeed(this.horse, this.course, this.phase);
 		}
 		if (!this.startDash) {
-			this.targetSpeed += this.activeSpeedSkills.reduce((a,b) => a + b.effect.modifier, 0);
+			this.targetSpeed += this.activeSpeedSkills.reduce((a,b) => a + b.modifier, 0);
 		}
 		if (this.hillIdx != -1) {
 			this.targetSpeed -= this.course.slopes[this.hillIdx].slope / 10000 * 200 / this.horse.power;
@@ -208,7 +217,7 @@ export class RaceSolver {
 		if (this.startDash) {
 			this.accel += 24.0;
 		}
-		this.accel += this.activeAccelSkills.reduce((a,b) => a + b.effect.modifier, 0);
+		this.accel += this.activeAccelSkills.reduce((a,b) => a + b.modifier, 0);
 	}
 
 	updateHills() {
@@ -226,39 +235,41 @@ export class RaceSolver {
 	}
 
 	processSkillActivations(dt: number) {
-		for (var i = this.activeSpeedSkills.length; --i >= 0;) {
+		for (let i = this.activeSpeedSkills.length; --i >= 0;) {
 			const s = this.activeSpeedSkills[i];
 			if ((s.remainingDuration -= dt) <= 0) {
 				this.activeSpeedSkills.splice(i,1);
-				this.onSkillDeactivate(s.effect);
+				this.onSkillDeactivate(s.skillId);
 			}
 		}
-		for (var i = this.activeAccelSkills.length; --i >= 0;) {
+		for (let i = this.activeAccelSkills.length; --i >= 0;) {
 			const s = this.activeAccelSkills[i];
 			if ((s.remainingDuration -= dt) <= 0) {
 				this.activeAccelSkills.splice(i,1);
-				this.onSkillDeactivate(s.effect);
+				this.onSkillDeactivate(s.skillId);
 			}
 		}
-		for (var i = this.pendingSkills.length; --i >= 0;) {
+		for (let i = this.pendingSkills.length; --i >= 0;) {
 			const s = this.pendingSkills[i];
-			if (this.pos >= s.trigger.end) { // NB. `Region`s are half-open [start,end) intervals. If pos == end we are out of the trigger.
+			if (this.pos >= s.trigger.end) {  // NB. `Region`s are half-open [start,end) intervals. If pos == end we are out of the trigger.
 				// skill failed to activate
 				this.pendingSkills.splice(i,1);
 			} else if (this.pos >= s.trigger.start && s.extraCondition(this)) {
-				const scaledDuration = s.effect.baseDuration * this.course.distance / 1000;
-				switch (s.effect.type) {
-				case SkillType.TargetSpeed:
-					this.activeSpeedSkills.push({remainingDuration: scaledDuration, effect: s.effect});
-					break;
-				case SkillType.Accel:
-					this.activeAccelSkills.push({remainingDuration: scaledDuration, effect: s.effect});
-					break;
-				case SkillType.CurrentSpeed:
-					this.currentSpeedModifier += s.effect.modifier;
-					break;
-				}
-				this.onSkillActivate(s.effect);
+				s.effects.forEach(ef => {
+					const scaledDuration = ef.baseDuration * this.course.distance / 1000;
+					switch (ef.type) {
+					case SkillType.TargetSpeed:
+						this.activeSpeedSkills.push({skillId: s.skillId, remainingDuration: scaledDuration, modifier: ef.modifier});
+						break;
+					case SkillType.Accel:
+						this.activeAccelSkills.push({skillId: s.skillId, remainingDuration: scaledDuration, modifier: ef.modifier});
+						break;
+					case SkillType.CurrentSpeed:
+						this.currentSpeedModifier += ef.modifier;
+						break;
+					}
+				});
+				this.onSkillActivate(s.skillId);
 				this.pendingSkills.splice(i,1);
 			}
 		}

tools/ToolCLI.ts
@@ -5,7 +5,7 @@ import { HorseParameters, Strategy, Aptitude } from '../HorseTypes';
 import { CourseData, CourseHelpers } from '../CourseData';
 import { Conditions, Region, RegionList, ActivationSamplePolicy } from '../ActivationConditions';
 import { parse, tokenize } from '../ConditionParser';
-import { DynamicCondition, SkillType, SkillEffect } from '../RaceSolver';
+import { DynamicCondition, SkillType, SkillRarity, SkillEffect } from '../RaceSolver';
 
 import skills from '../data/skill_data.json';
 
@@ -50,6 +50,8 @@ const GroundPowerModifier = Object.freeze([
 ].map(o => Object.freeze(o)));
 
 export interface SkillData {
+	skillId: string
+	rarity: SkillRarity
 	samplePolicy: ActivationSamplePolicy,
 	regions: RegionList,
 	extraCondition: DynamicCondition,
@@ -60,8 +62,8 @@ function buildSkillData(horse: HorseParameters, course: CourseData, wholeCourse:
 	if (!(skillId in skills)) {
 		throw new InvalidArgumentError('bad skill ID ' + skillId);
 	}
-	const alternatives = skills[skillId];
-	for (var i = 0; i < alternatives.length; ++i) {
+	const alternatives = skills[skillId].alternatives;
+	for (let i = 0; i < alternatives.length; ++i) {
 		const skill = alternatives[i];
 		if (skill.precondition) {
 			const pre = parse(tokenize(skill.precondition));
@@ -79,20 +81,23 @@ function buildSkillData(horse: HorseParameters, course: CourseData, wholeCourse:
 			var type: SkillType | -1 = -1;
 			switch (ef.type) {
 			case 21:  // debuffs
-				acc.push({skillId: skillId, type: SkillType.CurrentSpeed, baseDuration: skill.baseDuration / 10000, modifier: ef.modifier / 10000});
-				acc.push({skillId: skillId, type: SkillType.TargetSpeed, baseDuration: skill.baseDuration / 10000, modifier: ef.modifier / 10000});
+				acc.push({type: SkillType.CurrentSpeed, baseDuration: skill.baseDuration / 10000, modifier: ef.modifier / 10000});
+				acc.push({type: SkillType.TargetSpeed, baseDuration: skill.baseDuration / 10000, modifier: ef.modifier / 10000});
 				return acc;
 			case 22: type = SkillType.CurrentSpeed; break;
 			case 27: type = SkillType.TargetSpeed; break;
 			case 31: type = SkillType.Accel; break;
 			}
 			if (type != -1) {
-				acc.push({skillId: skillId, type: type, baseDuration: skill.baseDuration / 10000, modifier: ef.modifier / 10000});
+				acc.push({type: type, baseDuration: skill.baseDuration / 10000, modifier: ef.modifier / 10000});
 			}
 			return acc;
 		}, []);
 		if (effects.length > 0) {
 			return {
+				skillId: skillId,
+				// for some reason 1*/2* uniques, 1*/2* upgraded to 3*, and naturally 3* uniques all have different rarity (3, 4, 5 respectively)
+				rarity: Math.min(skills[skillId].rarity, 3),
 				samplePolicy: op.samplePolicy,
 				regions: regions,
 				extraCondition: extraCondition,

tools/dump.ts
@@ -6,11 +6,17 @@ import { SkillData, ToolCLI } from './ToolCLI';
 const cli = new ToolCLI();
 cli.run((horse: HorseParameters, course: CourseData, defSkills: SkillData[], cliSkills: SkillData[], cliOptions: any) => {
 	const s = new RaceSolver(horse, course);
+	const skillTypes = {};
 
-	function addSkill(sd) {
-		const trigger = sd.samplePolicy.sample(sd.regions, 1)[0];
-		s.pendingSkills.push.apply(s.pendingSkills,
-			sd.effects.map(ef => ({trigger: trigger, extraCondition: sd.extraCondition, effect: ef})));
+	function addSkill(sd: SkillData) {
+		skillTypes[sd.skillId] = sd.effects[0].type;
+		s.pendingSkills.push({
+			skillId: sd.skillId,
+			rarity: sd.rarity,
+			trigger: sd.samplePolicy.sample(sd.regions, 1)[0],
+			extraCondition: sd.extraCondition,
+			effects: sd.effects
+		});
 	}
 
 	defSkills.forEach(addSkill);
@@ -18,10 +24,10 @@ cli.run((horse: HorseParameters, course: CourseData, defSkills: SkillData[], cli
 
 	const plotData = {trackId: course.raceTrackId, courseId: cliOptions.course, t: [0], pos: [0], v: [0], targetv: [s.targetSpeed], a: [0], skills: {}};
 
-	s.onSkillActivate = (sk) => { plotData.skills[sk.skillId] = [sk.type,s.accumulatetime,0,s.pos,0]; }
-	s.onSkillDeactivate = (sk) => {
-		plotData.skills[sk.skillId][2] = s.accumulatetime;
-		plotData.skills[sk.skillId][4] = s.pos;
+	s.onSkillActivate = (skillId) => { plotData.skills[skillId] = [skillTypes[skillId],s.accumulatetime,0,s.pos,0]; }
+	s.onSkillDeactivate = (skillId) => {
+		plotData.skills[skillId][2] = s.accumulatetime;
+		plotData.skills[skillId][4] = s.pos;
 	}
 
 	while (s.pos < course.distance) {

tools/gain.ts
@@ -56,8 +56,12 @@ cli.run((horse: HorseParameters, course: CourseData, defSkills: SkillData[], cli
 	debuffs.forEach(addTriggers);
 
 	function addSkill(s: RaceSolver, sd: SkillData, triggers: Region[], i: number) {
-		sd.effects.forEach(ef => {
-			s.pendingSkills.push({trigger: triggers[i % triggers.length], extraCondition: sd.extraCondition, effect: ef});
+		s.pendingSkills.push({
+			skillId: sd.skillId,
+			rarity: sd.rarity,
+			trigger: triggers[i % triggers.length],
+			extraCondition: sd.extraCondition,
+			effects: sd.effects
 		});
 	}
 

commit 682cba831b17f552be4b5b8214ce7aa438ab2aca

fix a bug resulting in unique skill not being added to skill_data.json
introduced in 9c4f1f6 by the addition of is_general_skill to the db query in an attempt to filter out story-only skills

tools/make_skill_data.pl
@@ -31,7 +31,7 @@ SELECT id, rarity,
        ability_type_2_2, float_ability_value_2_2,
        ability_type_2_3, float_ability_value_2_3
   FROM skill_data
- WHERE is_general_skill = 1;
+ WHERE is_general_skill = 1 OR rarity >= 3;
 SQL
 );
 