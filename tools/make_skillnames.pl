b459210a23f21683c9609496f23053008e600147
pecan <turkchess123@gmail.com>
2022-08-22 07:35:48 -0600

    update skill names again
    now uses a file obtained via a gametora pseudo-API instead of scraping the skills page

--- tools/make_skillnames.pl
+++ tools/make_skillnames.pl
@@ -5,26 +5,24 @@ use utf8;
 use open qw(:encoding(UTF-8));
 binmode(STDOUT, ':encoding(UTF-8)');
 
-use Mojo::DOM;
 use JSON::PP;
-
-my $filename = shift @ARGV;
-my $dom = Mojo::DOM->new(do { local(@ARGV, $/) = $filename; <> });
-
-my $skills = {};
-
-for my $row ($dom->find('[class^=skills_table_row]')->each) {
-	if ($row->at('[class^=skills_table_desc]')->text =~ /\((\d+)\)/) {
-		my $id = $1;
-		my $enname = $row->at('[class^=skills_table_enname]')->text;
-		my $jpname = $row->at('[class^=skills_table_jpname]')->text;
-		$skills->{$id} = [$jpname,$enname];
-		if ($id =~ /^1(\d+)/) {  # add inherited versions of uniques
-			$skills->{'9' . $1} = [$jpname . '（継承）',$enname . ' (inherited)'];
-		}
-	}
-}
 
 my $json = JSON::PP->new;
 $json->canonical(1);
+
+my $filename = shift @ARGV;
+my $names = $json->decode(do { local(@ARGV, $/) = $filename; <> });
+
+my $skills = {};
+
+for my $skill (@$names) {
+	my $id = $skill->{id};
+	my $enname = $skill->{name_en};
+	my $jpname = $skill->{name_ja};
+	$skills->{$id} = [$jpname,$enname];
+	if ($id =~ /^1(\d+)/) {  # add inherited versions of uniques
+		$skills->{'9' . $1} = [$jpname . '（継承）',$enname . ' (inherited)'];
+	}
+}
+
 say $json->encode($skills);
