177208a889efa231f396b7921cb1e123787f363c
pecan <turkchess123@gmail.com>
2022-09-03 21:58:52 -0600

    switch names of opts/args for consistency

--- tools/plot.py
+++ tools/plot.py
@@ -11,20 +11,20 @@ matplotlib.rcParams['font.family'] = 'MS Gothic'
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
 
-opts = argparse.ArgumentParser(add_help=False)
-opts.add_argument('--help', action='help', help='show this help message and exit')
-opts.add_argument('--accel', '-a', action='store_true')
-opts.add_argument('--velocity', '-v', action='store_true')
-opts.add_argument('--target-velocity', '-r', action='store_true')
-opts.add_argument('--position', action='store_true')
-opts.add_argument('--velocity-offset', '-o', type=int, default=0)
-opts.add_argument('--hills', '-h', action='store_true')
-opts.add_argument('--corners', '-c', action='store_true')
-opts.add_argument('--straights', '-s', action='store_true')
-opts.add_argument('--phase', '-p', action='store_true')
-opts.add_argument('--skills', '-k', action='store_true')
-opts.add_argument('--lang', choices=('jp','en'), default='jp', help='language for skill names')
-args = opts.parse_args()
+args = argparse.ArgumentParser(add_help=False)
+args.add_argument('--help', action='help', help='show this help message and exit')
+args.add_argument('--accel', '-a', action='store_true')
+args.add_argument('--velocity', '-v', action='store_true')
+args.add_argument('--target-velocity', '-r', action='store_true')
+args.add_argument('--position', action='store_true')
+args.add_argument('--velocity-offset', '-o', type=int, default=0)
+args.add_argument('--hills', '-h', action='store_true')
+args.add_argument('--corners', '-c', action='store_true')
+args.add_argument('--straights', '-s', action='store_true')
+args.add_argument('--phase', '-p', action='store_true')
+args.add_argument('--skills', '-k', action='store_true')
+args.add_argument('--lang', choices=('jp','en'), default='jp', help='language for skill names')
+opts = args.parse_args()
 
 data = json.load(sys.stdin)
 
@@ -34,13 +34,13 @@ with open(os.path.join(DATA_DIR, 'course_data.json'), 'r', encoding='utf-8') as 
 
 plt.figure(figsize=(18,6))
 
-if args.accel:
+if opts.accel:
 	plt.plot(data['t'], data['a'], color='limegreen', label='Acceleration')
-if args.velocity:
+if opts.velocity:
 	plt.plot(data['t'], data['v'], color='navy', label='Current speed')
-if args.target_velocity:
+if opts.target_velocity:
 	plt.plot(data['t'], data['targetv'], color='cornflowerblue', label='Target speed')
-if args.position:
+if opts.position:
 	plt.plot(data['t'], data['pos'])
 
 def pos_to_t(pos):
@@ -49,7 +49,7 @@ def pos_to_t(pos):
 
 right = course['turn'] == 1
 
-if args.hills:
+if opts.hills:
 	for hill in course['slopes']:
 		if hill['slope'] < 0:
 			color = '#7dffbe'
@@ -67,20 +67,20 @@ if args.hills:
 		end_t = pos_to_t(hill['start'] + hill['length'])
 		plt.axvspan(start_t, end_t, color=color, alpha=0.3, hatch=hatch, label=label)
 
-if args.corners:
+if opts.corners:
 	for corner in course['corners']:
 		start_t = pos_to_t(corner['start'])
 		end_t = pos_to_t(corner['start'] + corner['length'])
 		orient = '↷' if right else '↶'
 		plt.axvspan(start_t, end_t, color='#e1beff', alpha=0.2, label=f"{orient} {corner['start']}m~{corner['start']+corner['length']}m")
 
-if args.straights:
+if opts.straights:
 	for straight in course['straights']:
 		start_t = pos_to_t(straight['start'])
 		end_t = pos_to_t(straight['end'])
 		plt.axvspan(start_t, end_t, color='#d1ebff', alpha=0.3, hatch='-', label=f"→ {straight['start']}m~{straight['end']}m")
 
-if args.phase:
+if opts.phase:
 	plt.axvline(pos_to_t(course['distance'] * 1/6), color='dimgray', alpha=0.2, ls='--', label=f"Mid leg start ({round(course['distance']*1/6)}m)")
 	plt.axvline(pos_to_t(course['distance'] * 2/3), color='dimgray', alpha=0.2, ls='--', label=f"Last leg start ({round(course['distance']*2/3)}m)")
 	plt.axvline(pos_to_t(course['distance'] * 5/6), color='dimgray', alpha=0.2, ls=':')
@@ -92,7 +92,7 @@ def get_skillname(id):
 	if id.startswith('pd'):
 		return 'Pace down'
 	if id in skillnames:
-		return skillnames[id][args.lang == 'en']
+		return skillnames[id][opts.lang == 'en']
 	return id
 
 skillcolors = {
@@ -108,7 +108,7 @@ skillcolors = {
 	31: 'orangered'   # Accel
 }
 
-if args.skills:
+if opts.skills:
 	xtr = plt.gca().get_xaxis_transform()
 
 	starts = sorted(list(map(lambda i: i[1], data['skills'].values())))
@@ -125,10 +125,10 @@ seconds = range(0, round(data['t'][-1])+1)
 plt.xticks(seconds, list(map(lambda t: '{:d}:{:02d}'.format(*divmod(t,60)), seconds)), rotation=45, rotation_mode='anchor', fontsize='xx-small', ha='right')
 
 plt.xlim([0, data['t'][-1]])
-if args.target_velocity:
-	plt.ylim([args.velocity_offset, max(data['targetv'])+1])
-elif args.velocity:
-	plt.ylim([args.velocity_offset, max(data['v'])+1])
+if opts.target_velocity:
+	plt.ylim([opts.velocity_offset, max(data['targetv'])+1])
+elif opts.velocity:
+	plt.ylim([opts.velocity_offset, max(data['v'])+1])
 	# NB. if they are both specified, max(targetv) >= max(v)
 
 plt.legend(loc='center left', bbox_to_anchor=(1,0.5))
