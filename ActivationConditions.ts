9ca828f674eb91b478f569348ea1d2f1a5480065
pecan <turkchess123@gmail.com>
2025-07-13 12:31:25 -0600

    implement != for is_dirtgrade
    used by parade rickey pink 109802211

--- ActivationConditions.ts
+++ ActivationConditions.ts
@@ -518,6 +518,10 @@ export const Conditions: {[cond: string]: Condition} = Object.freeze({
 		filterEq(regions: RegionList, flag: number, course: CourseData, _: HorseParameters, extra: RaceParameters) {
 			assert(flag == 1, 'must be is_dirtgrade==1');
 			return [10101, 10103, 10104, 10105].indexOf(course.raceTrackId) > -1 ? regions : new RegionList();
+		},
+		filterNeq(regions: RegionList, flag: number, course: CourseData, _: HorseParameters, extra: RaceParameters) {
+			assert(flag == 1, 'must be is_dirtgrade!=1');
+			return [10101, 10103, 10104, 10105].indexOf(course.raceTrackId) == -1 ? regions : new RegionList();
 		}
 	}),
 	is_finalcorner: immediate({
