f343476318fcc1bae51f1380c981af9ea7524bd0
pecan <turkchess123@gmail.com>
2025-07-15 15:57:05 -0600

    add some of the new(er) race parameters to the Race arbitrary
    i.e., weather, season, etc, and also order range now
    
    previously these wouldn't be generated at all. now generate them, but we still have to check for their existence in order to be backwards-compatible with old checkpoints which won't have them

--- test/arb/Race.ts
+++ test/arb/Race.ts
@@ -33,13 +33,26 @@ export function CourseId() {
 	return fc.constantFrom(...courseids).noBias().noShrink();
 }
 
+export function Mood() {
+	return fc.constantFrom<rparams.Mood>(-2, -1, 0, 1, 2).noBias();
+}
+
 export function GroundCondition() {
 	return fc.constantFrom(rparams.GroundCondition.Good, rparams.GroundCondition.Yielding, rparams.GroundCondition.Soft, rparams.GroundCondition.Heavy)
 	         .noBias();
 }
 
-export function Mood() {
-	return fc.constantFrom<rparams.Mood>(-2, -1, 0, 1, 2).noBias();
+export function Weather() {
+	return fc.constantFrom(rparams.Weather.Sunny, rparams.Weather.Cloudy, rparams.Weather.Rainy, rparams.Weather.Snowy).noBias();
+}
+
+export function Season() {
+	return fc.constantFrom(rparams.Season.Spring, rparams.Season.Summer, rparams.Season.Autumn, rparams.Season.Winter, rparams.Season.Sakura)
+	         .noBias();
+}
+
+export function Time() {
+	return fc.constantFrom(rparams.Time.Morning, rparams.Time.Midday, rparams.Time.Evening, rparams.Time.Night).noBias();
 }
 
 export function Stat() {
@@ -74,10 +87,18 @@ export function SkillList(max: number = 30) {
 
 export interface RaceParams {
 	seed: number
-	courseId: string
-	groundCondition: rparams.GroundCondition
-	mood: rparams.Mood
 	horse: build.HorseDesc
+	courseId: string
+	mood: rparams.Mood
+	groundCondition: rparams.GroundCondition
+	weather?: rparams.Weather  // these are all marked optional because they didn't used to exist and we want to be able to
+	season?: rparams.Season    // run old checkpoints, so we can't assume they're there
+	time?: rparams.Time
+	orderInfo?: {
+		orderRange?: [number,number]
+		numUmas: number
+		popularity: number
+	}
 	paceEffectsEnabled: boolean
 	nsamples: number
 	presupposedSkills: string[]
@@ -87,10 +108,24 @@ export interface RaceParams {
 export function Race({maxSamples = 200, maxPreSkills = 30, maxSut = 30}: {maxSamples?: number, maxPreSkills?: number, maxSut?: number} = {}) {
 	return fc.record({
 		seed: fc.integer().noBias().noShrink(),
-		courseId: CourseId(),
-		groundCondition: GroundCondition(),
-		mood: Mood(),
 		horse: HorseDesc(),
+		courseId: CourseId(),
+		mood: Mood(),
+		groundCondition: GroundCondition(),
+		weather: Weather(),
+		season: Season(),
+		time: Time(),
+		orderInfo: fc.tuple(
+			fc.boolean(),  // has order range info
+			fc.integer({min: 1, max: 18}),  // order range start
+			fc.integer({min: 0, max: 18}),  // order range length
+			fc.integer({min: 0, max: 18}),  // # umas after order range end (start + length + extra == numUmas)
+			fc.double({min: 0.0, max: 0.99, noNaN: true})  // % of numUmas more popular than us
+		).map(([hasOrder,start,length,extra,pop]) => ({
+			numUmas: start + length + extra,
+			orderRange: hasOrder ? [start, start + length] as [number,number] : void 0,
+			popularity: Math.floor((start + length + extra) * pop) + 1
+		})),
 		paceEffectsEnabled: fc.boolean(),
 		nsamples: fc.integer({min: 1, max: maxSamples}),
 		presupposedSkills: SkillList(maxPreSkills),
@@ -101,10 +136,19 @@ export function Race({maxSamples = 200, maxPreSkills = 30, maxSut = 30}: {maxSam
 export function makeBuilder(params: RaceParams) {
 	const builder = new build.RaceSolverBuilder(params.nsamples)
 		.seed(params.seed)
+		.horse(params.horse)
 		.course(+params.courseId)
-		.ground(params.groundCondition)
 		.mood(params.mood)
-		.horse(params.horse);
+		.ground(params.groundCondition);
+	if (params.weather != null) builder.weather(params.weather);
+	if (params.season != null) builder.season(params.season);
+	if (params.time != null) builder.time(params.time);
+	if (params.orderInfo != null) {
+		builder.numUmas(params.orderInfo.numUmas).popularity(params.orderInfo.popularity);
+		if (params.orderInfo.orderRange != null) {
+			builder.order(params.orderInfo.orderRange[0], params.orderInfo.orderRange[1]);
+		}
+	}
 	if (params.paceEffectsEnabled) {
 		builder.useDefaultPacer();
 	}
