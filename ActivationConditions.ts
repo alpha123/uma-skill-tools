alpha123/uma-skill-tools

commit 076c294c89f64d74dcb49f121372193da2b6aaeb

hack the corner_random skills to always activate on the first corner
see FIXME above the change for why we need to do this. this is pretty important in global at the moment since this includes a lot of common/significant skills (that are now all_corner_random==1 in jp)

ActivationConditions.ts
@@ -523,6 +523,22 @@ export const Conditions: {[cond: string]: Condition} = Object.freeze({
 	corner_random: random({
 		filterEq(regions: RegionList, cornerNum: number, course: CourseData, _: HorseParameters, extra: RaceParameters) {
 			assert(CourseHelpers.isSortedByStart(course.corners), 'course corners must be sorted by start');
+			// FIXME annoying hack for the corner skills. TEMPORARY. see the note above for why we do this. this condition is
+			// considerably more important in global than in jp (since early global does not have all_corner_random)
+			// these are all the corner_random==1@corner_random==2@corner_random==3@corner_random==4 skills
+			if ([
+				'200331', '200332', '200333', '200341', '200342', '200343', '200351', '200352', '200353',
+				'200971', '200972', '201041', '201042', '201111', '201112', '201181', '201182',
+				'201251', '201252', '201321', '201322', '201391', '201392', '201461', '201462'
+			].indexOf(extra.skillId) > -1) {
+				if (cornerNum == 1) {
+					const corner = course.corners[course.corners.length - 4];
+					const cornerBounds = new Region(corner.start, corner.start + corner.length);
+					return regions.rmap(r => r.intersect(cornerBounds));
+				} else {
+					return new RegionList();
+				}
+			}
 			if (course.corners.length + cornerNum >= 5) {
 				const corner = course.corners[course.corners.length + cornerNum - 5];
 				const cornerBounds = new Region(corner.start, corner.start + corner.length);

commit c3fdf083c79accbd5083772e60df546ea90bafc6

fix corner_random on tracks with < 4 corners
broken in 076c294c89f64d74dcb49f121372193da2b6aaeb

ActivationConditions.ts
@@ -532,7 +532,7 @@ export const Conditions: {[cond: string]: Condition} = Object.freeze({
 				'201251', '201252', '201321', '201322', '201391', '201392', '201461', '201462'
 			].indexOf(extra.skillId) > -1) {
 				if (cornerNum == 1) {
-					const corner = course.corners[course.corners.length - 4];
+					const corner = course.corners[Math.max(course.corners.length - 4, 0)];
 					const cornerBounds = new Region(corner.start, corner.start + corner.length);
 					return regions.rmap(r => r.intersect(cornerBounds));
 				} else {