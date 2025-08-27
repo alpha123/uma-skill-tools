fc7310c76fedbad07408ecf84a592a885b2720f5
pecan <turkchess123@gmail.com>
2025-08-27 01:59:57 -0600

    get jp skill names from db before adding english ones
    for cases where the gametora skill names file doesn't contain some, which has happened recently

--- tools/make_skillnames.pl
+++ tools/make_skillnames.pl
@@ -5,20 +5,41 @@ use utf8;
 use open qw(:encoding(UTF-8));
 binmode(STDOUT, ':encoding(UTF-8)');
 
+use DBI;
+use DBD::SQLite::Constants qw(:file_open);
 use JSON::PP;
+use Encoding qw(encode decode);
 
 my $json = JSON::PP->new;
 $json->canonical(1);
 
+my $mastermdb = shift @ARGV;
 my $filename = shift @ARGV;
 my $names = $json->decode(do { local(@ARGV, $/) = $filename; <> });
 
+my $db = DBI->connect("dbi:SQLite:$mastermdb", undef, undef, {
+	sqlite_open_flags => SQLITE_OPEN_READONLY
+});
+$db->{RaiseError} = 1;
+
+my $select = $db->prepare('SELECT [index], text FROM text_data WHERE category = 47;');
+
+$select->execute;
+
+my ($id, $utf8name);
+
+$select->bind_columns(\($id, $utf8name));
+
 my $skills = {};
 
+my %ennames;
 for my $skill (@$names) {
-	my $id = $skill->{id};
-	my $enname = $skill->{name_en};
-	my $jpname = $skill->{name_ja};
+	$ennames{$skill->{id}} = $skill->{name_en};
+}
+
+while ($select->fetch) {
+	my $jpname = Encode::decode('utf8', $utf8name);
+	my $enname = $ennames{$id} || '';
 
 	if ($id eq '100701') {  # Seirios
 		$skills->{'100701-1'} = [$jpname . '（人気4番以下）', $enname . ' (popularity 4 or lower)'];
